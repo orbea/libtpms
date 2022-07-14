tarak-stratus/libtpms_trialpolicy_utils

tpm2: Implement functions to set and check runtime-disabled commands
Signed-off-by: Stefan Berger <stefab@linux.ibm.com>

src/tpm2/RuntimeCommands_fp.h
@@ -1,0 +1,95 @@
+/********************************************************************************/
+/*										*/
+/*			 TPM 2 Commands Runtime Disablement			*/
+/*			     Written by Stefan Berger				*/
+/*		       IBM Thomas J. Watson Research Center			*/
+/*										*/
+/*  Licenses and Notices							*/
+/*										*/
+/*  1. Copyright Licenses:							*/
+/*										*/
+/*  - Trusted Computing Group (TCG) grants to the user of the source code in	*/
+/*    this specification (the "Source Code") a worldwide, irrevocable, 		*/
+/*    nonexclusive, royalty free, copyright license to reproduce, create 	*/
+/*    derivative works, distribute, display and perform the Source Code and	*/
+/*    derivative works thereof, and to grant others the rights granted herein.	*/
+/*										*/
+/*  - The TCG grants to the user of the other parts of the specification 	*/
+/*    (other than the Source Code) the rights to reproduce, distribute, 	*/
+/*    display, and perform the specification solely for the purpose of 		*/
+/*    developing products based on such documents.				*/
+/*										*/
+/*  2. Source Code Distribution Conditions:					*/
+/*										*/
+/*  - Redistributions of Source Code must retain the above copyright licenses, 	*/
+/*    this list of conditions and the following disclaimers.			*/
+/*										*/
+/*  - Redistributions in binary form must reproduce the above copyright 	*/
+/*    licenses, this list of conditions	and the following disclaimers in the 	*/
+/*    documentation and/or other materials provided with the distribution.	*/
+/*										*/
+/*  3. Disclaimers:								*/
+/*										*/
+/*  - THE COPYRIGHT LICENSES SET FORTH ABOVE DO NOT REPRESENT ANY FORM OF	*/
+/*  LICENSE OR WAIVER, EXPRESS OR IMPLIED, BY ESTOPPEL OR OTHERWISE, WITH	*/
+/*  RESPECT TO PATENT RIGHTS HELD BY TCG MEMBERS (OR OTHER THIRD PARTIES)	*/
+/*  THAT MAY BE NECESSARY TO IMPLEMENT THIS SPECIFICATION OR OTHERWISE.		*/
+/*  Contact TCG Administration (admin@trustedcomputinggroup.org) for 		*/
+/*  information on specification licensing rights available through TCG 	*/
+/*  membership agreements.							*/
+/*										*/
+/*  - THIS SPECIFICATION IS PROVIDED "AS IS" WITH NO EXPRESS OR IMPLIED 	*/
+/*    WARRANTIES WHATSOEVER, INCLUDING ANY WARRANTY OF MERCHANTABILITY OR 	*/
+/*    FITNESS FOR A PARTICULAR PURPOSE, ACCURACY, COMPLETENESS, OR 		*/
+/*    NONINFRINGEMENT OF INTELLECTUAL PROPERTY RIGHTS, OR ANY WARRANTY 		*/
+/*    OTHERWISE ARISING OUT OF ANY PROPOSAL, SPECIFICATION OR SAMPLE.		*/
+/*										*/
+/*  - Without limitation, TCG and its members and licensors disclaim all 	*/
+/*    liability, including liability for infringement of any proprietary 	*/
+/*    rights, relating to use of information in this specification and to the	*/
+/*    implementation of this specification, and TCG disclaims all liability for	*/
+/*    cost of procurement of substitute goods or services, lost profits, loss 	*/
+/*    of use, loss of data or any incidental, consequential, direct, indirect, 	*/
+/*    or special damages, whether under contract, tort, warranty or otherwise, 	*/
+/*    arising in any way out of use or reliance upon this specification or any 	*/
+/*    information herein.							*/
+/*										*/
+/*  (c) Copyright IBM Corp. and others, 2022					*/
+/*										*/
+/********************************************************************************/
+
+#ifndef RUNTIME_COMMANDS_H
+#define RUNTIME_COMMANDS_H
+
+#include "Tpm.h"
+
+#define CC_OFFSET TPM_CC_NV_UndefineSpaceSpecial  /* lowest command code number */
+
+#define CcToIdx(COC) (size_t)(COC - CC_OFFSET)
+#define IdxToCc(IDX) (TPM_CC)(IDX + CC_OFFSET)
+
+#define NUM_ENTRIES_COMMAND_PROPERTIES  (CcToIdx(TPM_CC_ECC_Decrypt) + 1)
+
+struct RuntimeCommands {
+    unsigned char enabledCommands[(IdxToCc(NUM_ENTRIES_COMMAND_PROPERTIES) + 7) / 8];
+    char *commandsProfile;
+};
+
+void
+RuntimeCommandsInit(
+		    struct RuntimeCommands *RuntimeCommands
+		    );
+
+TPM_RC
+RuntimeCommandsSetProfile(
+                          struct RuntimeCommands *RuntimeCommands,
+			  const char             *newProfile
+			  );
+
+BOOL
+RuntimeCommandsCheckEnabled(
+			    struct RuntimeCommands *RuntimeCommands,
+			    TPM_CC		    cc      // IN: the command code to check
+			    );
+
+#endif /* RUNTIME_COMMANDS_H */

orbea/libtpms

src/tpm2/RuntimeCommands_fp.h
@@ -1,0 +1,100 @@
+/********************************************************************************/
+/*										*/
+/*			 TPM 2 Commands Runtime Disablement			*/
+/*			     Written by Stefan Berger				*/
+/*		       IBM Thomas J. Watson Research Center			*/
+/*										*/
+/*  Licenses and Notices							*/
+/*										*/
+/*  1. Copyright Licenses:							*/
+/*										*/
+/*  - Trusted Computing Group (TCG) grants to the user of the source code in	*/
+/*    this specification (the "Source Code") a worldwide, irrevocable, 		*/
+/*    nonexclusive, royalty free, copyright license to reproduce, create 	*/
+/*    derivative works, distribute, display and perform the Source Code and	*/
+/*    derivative works thereof, and to grant others the rights granted herein.	*/
+/*										*/
+/*  - The TCG grants to the user of the other parts of the specification 	*/
+/*    (other than the Source Code) the rights to reproduce, distribute, 	*/
+/*    display, and perform the specification solely for the purpose of 		*/
+/*    developing products based on such documents.				*/
+/*										*/
+/*  2. Source Code Distribution Conditions:					*/
+/*										*/
+/*  - Redistributions of Source Code must retain the above copyright licenses, 	*/
+/*    this list of conditions and the following disclaimers.			*/
+/*										*/
+/*  - Redistributions in binary form must reproduce the above copyright 	*/
+/*    licenses, this list of conditions	and the following disclaimers in the 	*/
+/*    documentation and/or other materials provided with the distribution.	*/
+/*										*/
+/*  3. Disclaimers:								*/
+/*										*/
+/*  - THE COPYRIGHT LICENSES SET FORTH ABOVE DO NOT REPRESENT ANY FORM OF	*/
+/*  LICENSE OR WAIVER, EXPRESS OR IMPLIED, BY ESTOPPEL OR OTHERWISE, WITH	*/
+/*  RESPECT TO PATENT RIGHTS HELD BY TCG MEMBERS (OR OTHER THIRD PARTIES)	*/
+/*  THAT MAY BE NECESSARY TO IMPLEMENT THIS SPECIFICATION OR OTHERWISE.		*/
+/*  Contact TCG Administration (admin@trustedcomputinggroup.org) for 		*/
+/*  information on specification licensing rights available through TCG 	*/
+/*  membership agreements.							*/
+/*										*/
+/*  - THIS SPECIFICATION IS PROVIDED "AS IS" WITH NO EXPRESS OR IMPLIED 	*/
+/*    WARRANTIES WHATSOEVER, INCLUDING ANY WARRANTY OF MERCHANTABILITY OR 	*/
+/*    FITNESS FOR A PARTICULAR PURPOSE, ACCURACY, COMPLETENESS, OR 		*/
+/*    NONINFRINGEMENT OF INTELLECTUAL PROPERTY RIGHTS, OR ANY WARRANTY 		*/
+/*    OTHERWISE ARISING OUT OF ANY PROPOSAL, SPECIFICATION OR SAMPLE.		*/
+/*										*/
+/*  - Without limitation, TCG and its members and licensors disclaim all 	*/
+/*    liability, including liability for infringement of any proprietary 	*/
+/*    rights, relating to use of information in this specification and to the	*/
+/*    implementation of this specification, and TCG disclaims all liability for	*/
+/*    cost of procurement of substitute goods or services, lost profits, loss 	*/
+/*    of use, loss of data or any incidental, consequential, direct, indirect, 	*/
+/*    or special damages, whether under contract, tort, warranty or otherwise, 	*/
+/*    arising in any way out of use or reliance upon this specification or any 	*/
+/*    information herein.							*/
+/*										*/
+/*  (c) Copyright IBM Corp. and others, 2022					*/
+/*										*/
+/********************************************************************************/
+
+#ifndef RUNTIME_COMMANDS_H
+#define RUNTIME_COMMANDS_H
+
+#include "Tpm.h"
+
+#define CC_OFFSET    TPM_CC_First
+
+#define CcToIdx(COC) (size_t)(COC - CC_OFFSET)
+#define IdxToCc(IDX) (TPM_CC)(IDX + CC_OFFSET)
+
+#define NUM_ENTRIES_COMMAND_PROPERTIES  (CcToIdx(TPM_CC_Last) + 1)
+
+struct RuntimeCommands {
+    unsigned char enabledCommands[(IdxToCc(NUM_ENTRIES_COMMAND_PROPERTIES) + 7) / 8];
+    char *commandsProfile;
+};
+
+void
+RuntimeCommandsInit(
+		    struct RuntimeCommands *RuntimeCommands
+		    );
+
+TPM_RC
+RuntimeCommandsSetProfile(
+			  struct RuntimeCommands *RuntimeCommands,
+			  const char             *newProfile
+			  );
+
+BOOL
+RuntimeCommandsCheckEnabled(
+			    struct RuntimeCommands *RuntimeCommands,
+			    TPM_CC		    cc      // IN: the command code to check
+			    );
+
+LIB_EXPORT UINT32
+RuntimeCommandsCountEnabled(
+			    struct RuntimeCommands *RuntimeCommands
+			    );
+
+#endif /* RUNTIME_COMMANDS_H */