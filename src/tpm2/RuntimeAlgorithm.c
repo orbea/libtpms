tarak-stratus/libtpms_trialpolicy_utils

tpm2: Allow to runtime-disable SM4 symmetric encryption algorithm
Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

src/tpm2/RuntimeAlgorithm.c
@@ -143,7 +143,7 @@ static const struct algname_to_id {
     [TPM_ALG_SHA384] = HASH(ALG_SHA384, "sha384", false),
     [TPM_ALG_SHA512] = HASH(ALG_SHA512, "sha512", false),
     [TPM_ALG_NULL] = OTHER(true, "null", false),
-    [TPM_ALG_SM4] = SYMMETRIC(ALG_SM4, "sm4", key_sizes_sm4, false),
+    [TPM_ALG_SM4] = SYMMETRIC(ALG_SM4, "sm4", key_sizes_sm4, true),
     [TPM_ALG_RSASSA] = SIGNING(ALG_RSASSA, "rsassa", false),
     [TPM_ALG_RSAES] = ENCRYPTING(ALG_RSAES, "rsaes", false),
     [TPM_ALG_RSAPSS] = SIGNING(ALG_RSAPSS, "rsapss", false),

orbea/libtpms

src/tpm2/RuntimeAlgorithm.c
@@ -147,7 +147,7 @@ static const struct {
     [TPM_ALG_SHA384] = HASH(ALG_SHA384, "sha384", false),
     [TPM_ALG_SHA512] = HASH(ALG_SHA512, "sha512", false),
     [TPM_ALG_NULL] = OTHER(true, "null", false),
-    [TPM_ALG_SM4] = SYMMETRIC(ALG_SM4, "sm4", s_KeySizesSM4, false),
+    [TPM_ALG_SM4] = SYMMETRIC(ALG_SM4, "sm4", s_KeySizesSM4, true),
     [TPM_ALG_RSASSA] = SIGNING(ALG_RSASSA, "rsassa", false),
     [TPM_ALG_RSAES] = ENCRYPTING(ALG_RSAES, "rsaes", false),
     [TPM_ALG_RSAPSS] = SIGNING(ALG_RSAPSS, "rsapss", false),